444bbbc8fab645dea1e66de27c08191ed1b132ac
James Beissel <james.beissel@wildlifeprotectionsolutions.org>
2019-05-17 11:17:55 -0600

    Added TestService

---
+++ ClassLibrary1/TestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class TestService : ITestService
+    {
+        public void ThrowError()
+        {
+            var message = $"Test Exception";
+
+            throw new Exception(message);
+        }
+    }
+}
