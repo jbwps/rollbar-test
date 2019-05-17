f485c0f65bb7ead7a05bc3aae7afd9b59d58d252
James Beissel <james.beissel@wildlifeprotectionsolutions.org>
2019-05-17 11:14:59 -0600

    Initial Commit

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ITestService _testService;
+
+        public TestController(ITestService testService)
+        {
+            _testService = testService;
+        }
+
+        // GET api/test
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello World";
+        }
+
+        // Example 1
+        // Actual: A Rollbar item with title "Rollbar.NetCore.AspNet.RollbarMiddlewareException: The included internal exception processed by the Rollbar middleware"
+        // Expected: A Rollbar item with title "System.Exception: Test Exception"
+
+        // POST api/test
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            var message = $"Test Exception";
+
+            throw new Exception(message);
+        }
+
+        // Example 2
+        // Actual: Two Rollbar items with titles "RollbarMiddleware processed uncaught exception." and "The included internal exception processed by the Rollbar middleware"
+        // Expected: A Single Rollbar item with title "System.Exception: Test Exception"
+        // PUT api/test
+        [HttpPut]
+        public async Task<IActionResult> Put()
+        {
+            _testService.ThrowError();
+
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,54 @@
+﻿using ClassLibrary1;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Rollbar;
+using Rollbar.NetCore.AspNet;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        private const string RollbarAccessToken = "Your Token Here";
+        private const string RollbarEnvironment = "Development";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            RollbarLocator.RollbarInstance
+               .Configure(new RollbarConfig(RollbarAccessToken)
+               {
+                   Environment = RollbarEnvironment,
+                   Enabled = true
+               });
+
+            services.AddRollbarLogger(loggerOptions =>
+            {
+                loggerOptions.Filter = (loggerName, loglevel) => loglevel >= LogLevel.Error;
+            });
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddSingleton<ITestService, TestService>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseRollbarMiddleware();
+
+            app.UseMvc();
+        }
+    }
+}
